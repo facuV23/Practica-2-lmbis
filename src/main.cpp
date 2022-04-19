1d43924ecb463dcd996970a761e7040104562834
Facundo Viacava <facundoviacava@gmail.com>
2022-04-19 20:42:02 -0300

    funcion sumar

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "matematica.h"
 
 using namespace std;
 
@@ -6,5 +7,6 @@ int main (void){
 
 cout<<"hola como va"<<endl;
 
-
+int16_t x[] = { 1, 3, 2, 2 };
+cout<<Sumar_Array(x,4)<<endl;
 }
