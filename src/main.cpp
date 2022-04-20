cdec11d1678bedafb71549ea4be9d6f159637cb2
Facundo Viacava <facundoviacava@gmail.com>
2022-04-20 12:28:58 -0300

    agregue funcion sumar y prueba en el main

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,18 @@ using namespace std;
 
 int main (void){
 
-cout<<"hola como va"<<endl;
 
-int16_t x[] = { 1, 3, 2, 2 };
-cout<<Sumar_Array(x,4)<<endl;
+int16_t n=0;
+
+
+    cout<<"querio sumar 1,1,1,1"<<endl;
+    int16_t x[]={1,1,1,1};
+    cout<<"resultado:"<<Sumar_Array(x,4)<<endl;
+
+    cout<<"querio sumar 1,1,1,1,2,6,-10"<<endl;
+    int16_t x1[]={1,1,1,1,2,6,-10};
+    cout<<"resultado:"<<Sumar_Array(x1,7)<<endl;
+    
+    
+
 }
